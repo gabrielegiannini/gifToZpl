31040bb1631f35948ba6b6348b34c0bdb7af16b3
Gabriele <gabrielegiannini2@gmail.com>
2022-12-24 10:53:14 +0100

    Update gifToZpl.ts

--- gifToZpl.ts
+++ gifToZpl.ts
@@ -1,17 +1,19 @@
 import omggif from "omggif";
 const rgbaToZ64 = require('zpl-image').rgbaToZ64;
 
-/* Create a Buffer starting from a base64 encoded GIF */
-let buf = Buffer.from(data, 'base64');
-/* Create a GifReader (with our GIF as input) to use the functions provided by omggif */
-let gif = new omggif.GifReader(buf);
+function(data: string):string {
+  /* Create a Buffer starting from a base64 encoded GIF */
+  let buf = Buffer.from(data, 'base64');
+  /* Create a GifReader (with our GIF as input) to use the functions provided by omggif */
+  let gif = new omggif.GifReader(buf);
 
-/* Creation of the RGBA frame of our GIF */
-let rgba = Buffer.alloc(gif.width * gif.height * 4);
-gif.decodeAndBlitFrameRGBA(0, rgba);
+  /* Creation of the RGBA frame of our GIF */
+  let rgba = Buffer.alloc(gif.width * gif.height * 4);
+  gif.decodeAndBlitFrameRGBA(0, rgba);
 
-/* Conversion to Z64*/
-let res = rgbaToZ64(rgba, gif.width, { black:47, rotate: 'R' });
-/* ZPL costruction */
-let zpl = `^XA^LH0,0^FO0,0^GFA,${res.length},${res.length},${res.rowlen},${res.z64}^FS^XZ`;
-console.log(zpl);
+  /* Conversion to Z64*/
+  let res = rgbaToZ64(rgba, gif.width, { black:47, rotate: 'R' });
+  /* ZPL costruction */
+  let zpl = `^XA^LH0,0^FO0,0^GFA,${res.length},${res.length},${res.rowlen},${res.z64}^FS^XZ`;
+  return zpl;
+}
